af18261e68333d1a0714ced4f83dee7f00920481
Austin Burke <austinlayneburke@gmail.com>
2021-10-28 07:20:59 -0500

    Set up server side data retrieval

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,36 @@ import React, { Component } from "react";
 import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
+  static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-    console.log(campaigns);
+    return { campaigns };
   }
 
   render() {
-    return <div>Campaigns Index!</div>;
+    return <div>{this.props.campaigns[0]}</div>;
   }
 }
 
 export default CampaignIndex;
+
+/*
+ *   REFERENCE
+ */
+// const CampaignIndex = ({ campaigns }) => {
+//   console.log(campaigns);
+
+//   return <div>{campaigns && campaigns}</div>;
+// };
+
+// // `getInitialProps` enables server-side rendering in a page and
+// //    allows you to do initial data population, it means sending
+// //    the page with the data already populated from the server.
+// //    This is especially useful for SEO.
+// //    https://nextjs.org/docs/api-reference/data-fetching/getInitialProps
+// CampaignIndex.getInitialProps = async (ctx) => {
+//   const res = await factory.methods.getDeployedCampaigns().call();
+//   return { campaigns: res };
+// };
+
+// export default CampaignIndex;
