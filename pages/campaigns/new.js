8c776deb8d679f8b8317d4a71dbf8b00e4516183
Austin Burke <austinlayneburke@gmail.com>
2021-10-30 16:05:53 -0500

    Added submit fn for minimum contribution

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
 import { Form, Button, Input } from "semantic-ui-react";
 import Layout from "../../components/Layout";
+import factory from "../../ethereum/factory";
+import web3 from "../../ethereum/web3";
 
 class CampaignNew extends Component {
   state = {
     minimumContribution: "",
+    errorMessage: "",
+  };
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0],
+        });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
   };
 
   render() {
     return (
       <Layout>
-        <h3>Create a Campaign</h3>
-
-        <Form>
+        <h3>Create Campaign</h3>
+        <Form onSubmit={this.onSubmit}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -24,7 +41,6 @@ class CampaignNew extends Component {
               }
             />
           </Form.Field>
-
           <Button primary>Create!</Button>
         </Form>
       </Layout>
